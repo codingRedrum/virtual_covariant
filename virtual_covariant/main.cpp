c14fd4dbc80cb20313f745d2d9bfd062aa2b1f5c
codingRedrum <adrianjuszczak@icloud.com>
2021-01-06 19:28:02 +0100

    Initial Commit

---
+++ virtual_covariant/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  virtual_covariant
+//
+//  Created by Adrian on 06/01/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
